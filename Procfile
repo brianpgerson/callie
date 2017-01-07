c194a413102a91dbcf62055b73bf9d63ef43669e
Brian Gerson <brianpgerson@gmail.com>
2017-01-06 20:58:00 -0800

    Made a bot.

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: node bin/bot.js
---
+++ bin/bot.js
@@ -0,0 +1,14 @@
+'use strict';
+require('dotenv').config();
+
+var CountdownBot = require('../lib/countdown');
+
+var token = process.env.BOT_API_KEY;
+
+var thecount = new CountdownBot({
+    token: token,
+    name: 'thecount',
+    dateString: '3/9/2017'
+});
+
+thecount.run();

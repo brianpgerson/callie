fe57b7c07489d21bd0abe579c0a63ea169959535
Brian Gerson <brianpgerson@gmail.com>
2017-01-08 17:31:45 -0800

    Add schedule changer

--- bin/bot.js
+++ bin/bot.js
@@ -1,27 +1,21 @@
 'use strict';
 
 // I use dotenv to manage config vars. remove below if you do not.
-// require('dotenv').config();
+require('dotenv').config();
 
 var CountdownBot = require('../lib/countdown');
 var cities = require('../data/cities');
 var token = process.env.BOT_API_KEY;
-var schedule = require('node-schedule');
 var _ = require('lodash');
 
 var thecount = new CountdownBot({
     token: token,
     name: 'thecount',
-    destination: cities,
-    event: 'Mystery Trip',
-    date: '2017-03-09'
-});
-
-schedule.scheduleJob('* * 10 * 1', function(){
-  	_.each(thecount.channels, function(channel) {
-  		var message = `Weekly update: ${thecount._generateResponse()}`;
-  		thecount.postMessage(channel, message, {as_user: thecount.user.name});
-  	});
+    destination: 'your awesome place',
+    event: 'New Years Eve',
+    date: '2017-12-31',
+    chron: '* * 10 * * 1'   // see https://www.npmjs.com/package/node-schedule
+    				    	// for formatting info on chron rules
 });
 
 thecount.run();
--- data/welcomeMessage.js
+++ data/welcomeMessage.js
@@ -2,11 +2,22 @@ module.exports = `I'm a friendly countdown bot, and I'm very simple to use.
 Simply call my name (\`@thecount\`) and I'll tell you how many days are left until an event or trip to a destination of your choosing.
 Each countdown just needs either a destination, an event name, or both, plus the date.
 
-To reset, simply say:
+To set (or reset) your countdown, simply say:
 
 \`@thecount reset date: <the date, formatted YYYY-MM-DD>, destination: <your destination>, event: <your event name>\`
 
 I'll let you know if you got it right!
+
+I also give weekly or daily countdown reminders. You can set those by saying:
+
+\`@thecount schedule: <weekly or daily>, day: <day of week>, hour: <hour in military time>\`
+
+If you set it for daily, simply ignore the day setting, like so:
+
+\`@thecount schedule: daily, hour: <hour in military time>\`
+
+Hour is optional as well, and will default to 10am (Pacific).
+
 To see this message again, simply say "@thecount help".
 
 You can find more information (or contribute to The Count) at www.github.com/brianpgerson/the-count

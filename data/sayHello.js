f7961e5c2e6476ff5ab40d9705f49b8e80313720
Brian Gerson <brianpgerson@gmail.com>
2017-01-24 11:04:38 -0800

    update hellow message spacing

--- data/sayHello.js
+++ data/sayHello.js
@@ -1,7 +1,5 @@
-module.exports = `Hi there! I'm Callie the Calendar Corgi, a slack bot for keeping track of fun events, deadlines, and goals
-for you and your team. I'm also a very good dog!
+module.exports = `Hi there! I'm Callie the Calendar Corgi, a slack bot for keeping track of fun events, deadlines, and goals for you and your team. I'm also a very good dog!
 
-Get started and create your first countdown by saying \`@callie start date: <YYYY-MM-DD>, event: <event name>\`, and
-ask me about countdowns you create by saying \`@callie countdown: <event name>\`.
+Get started and create your first countdown by saying \`@callie start date: <YYYY-MM-DD>, event: <event name>\`, and ask me about countdowns you create by saying \`@callie countdown: <event name>\`.
 
 Ask for help if you need it, otherwise, I'll be hangin' around, just doing, you know, impawtent dog stuff.`

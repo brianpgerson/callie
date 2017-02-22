fea7b6d099a6f096613144cb7386022ad26181dc
Brian Gerson <brianpgerson@gmail.com>
2017-02-21 21:51:46 -0800

    Try a new message generator

--- data/enders.js
+++
@@ -1,6 +0,0 @@
-module.exports = [
-    'until your incredibly exciting',
-    'until your mind-bogglingly fun',
-    'until your super anticipated',
-    'until your epic and excellent'
-];
--- data/openers.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-	'Grrrrr... er, glad you asked! There are',
-	'Let me check the schedule. Ah, yes:',
-	'How exciting. There are only',
-	'Well if you must know...BARK! There remain',
-	'Arf! As far as I know, there are precisely'
-];

820172266644b8ac3bb241da35d7281d24b8641e
Brian Gerson <brianpgerson@gmail.com>
2017-02-21 21:52:57 -0800

    Remove unused modules

--- data/presets.js
+++ data/presets.js
@@ -3,13 +3,9 @@
 const help = require('./help'),
 	  sayHello = require('./sayHello'),
 	  cities = require('./cities'),
-	  openers = require('./openers'),
-	  enders = require('./enders');
 
 module.exports = {
 	help: help,
 	sayHello: sayHello,
-	cities: cities,
-	openers: openers,
-	enders: enders
+	cities: cities
 }

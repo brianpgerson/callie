6e43a2538cc7a459edb14b96a6a245964530835a
Brian Gerson <brianpgerson@gmail.com>
2017-04-10 12:02:28 -0700

    Some debugging shit

---
+++ data/nodumb.js
@@ -0,0 +1,13 @@
+module.exports = `No, no no...like it says in the help and welcome messages, there is no need for an actual \`<\` or \`>\`.
+
+That's just how I tell you to use your own inputs there. So when the example is \`@callie start date: <your date>, event: <your event>\`, do something like this:
+
+"@callie start date: 2040-05-11, event: flight to mars".
+
+Don't do something like this:
+
+@callie start date: <2040-05-11>, event: <flight to mars>.
+
+Why tell you this instead of just scrubbing the offending brackets? Because then you'd ask for countdowns to <flight to mars> and there wouldn't be anything there. This is easier. Trust me.
+
+If you have a suggestion for a better way to delineate "things that are your own inputs", please, tell me: countdown.to.counttown@gmail.com.`
--- data/presets.js
+++ data/presets.js
@@ -2,10 +2,12 @@
 
 const help = require('./help'),
 	  sayHello = require('./sayHello'),
+	  nodumb = require('./nodumb'),
 	  cities = require('./cities');
 
 module.exports = {
 	help: help,
 	sayHello: sayHello,
+	nodumb: nodumb,
 	cities: cities
 }

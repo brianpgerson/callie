4f9cf406a2a44f40a5fb7b62a9fcf88cd2d0b22e
Brian Gerson <brianpgerson@gmail.com>
2017-09-23 15:10:22 -0700

    Update log for error

--- bin/bot.js
+++ bin/bot.js
@@ -64,7 +64,7 @@ function restartBots () {
 			}
 		});
 	}).catch(function(err) {
-		console.log('error during boot:', err, countdown.botId);
+		console.log('error during boot:', err, bot.teamId, bot.teamName);
 	});
 }
 

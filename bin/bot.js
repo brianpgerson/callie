9b0992b7f83fac4dfac14e3dba5c1502ae7e6146
Brian Gerson <brianpgerson@gmail.com>
2018-02-11 13:32:43 -0800

    Try events api?

--- bin/bot.js
+++ bin/bot.js
@@ -1,106 +1,53 @@
 'use strict';
 
 // I use dotenv to manage config vars. remove below if you do not.
-// require('dotenv').config();
+require('dotenv').config();
 
-const CountdownBot = require('../lib/countdown'),
-			cities = require('../data/cities'),
-		  mongoose = require('mongoose'),
-			router = require('../router'),
-			   Bot = require('../lib/models/bot'),
-				 _ = require('lodash'),
-			 Relax = require('relax-js'),
-		 Countdown = require('../lib/models/countdown'),
-		   express = require('express'),
-			 utils = require('../lib/utils'),
-			 Redis = require('redis'),
-			   app = express();
+const 	   CountdownBot = require('../lib/countdown'),
+				 cities = require('../data/cities'),
+		  	   mongoose = require('mongoose'),
+				 router = require('../router'),
+			   		Bot = require('../lib/models/bot'),
+			 bodyParser = require('body-parser'),
+	  				 _  = require('lodash'),
+createSlackEventAdapter = require('@slack/events-api').createSlackEventAdapter,
+			slackEvents = createSlackEventAdapter(process.env.SLACK_VERIFICATION_TOKEN),
+		 	  Countdown = require('../lib/models/countdown'),
+		   	    express = require('express'),
+			 	  utils = require('../lib/utils'),
+			   		app = express();
 
 
+// initial setup
 mongoose.Promise = require('bluebird');
-const testDb = `mongodb://${process.env.TEST_MONGO_USER}:${process.env.TEST_MONGO_PASSWORD}@ds147274.mlab.com:47274/callie-test`;
-const prodDb = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@ds123991-a0.mlab.com:23991,ds123991-a1.mlab.com:23991/callie?replicaSet=rs-ds123991`;
+const testDb = process.env.TEST_DB;
+const prodDb = process.env.PROD_DB;
+
+// database setup
 const databaseUrl = isTestMode() ? testDb : prodDb;
+console.log(databaseUrl)
 mongoose.connect(databaseUrl);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log(`connected to DB in ${isTestMode() ? 'test mode' : 'live mode'}`);
-});
+db.once('open', () => console.log(`Connected to DB in ${isTestMode() ? 'test mode' : 'live mode'}`));
 
+// express setup
+app.use(bodyParser.json());
+app.use('/slack/events', slackEvents.expressMiddleware());
 app.use("/public", express.static(__dirname));
-
-app.listen(process.env.PORT || 1337, function(){
-  console.log(`Express server listening on port ${this.address().port}`);
+app.listen(process.env.PORT || 1337, function () {
+	console.log(`Server listening on port ${this.address().port}`)
 });
 
-const relax = new Relax();
+// business logic begins
 const countdownBot = new CountdownBot();
-// console.log(`Creating redis connection at ${process.env.REDIS_URL}`);
-// const redis = Redis.createClient({
-// 	url: process.env.REDIS_URL
-// });
-
-setup(countdownBot);
+router(app, db, countdownBot, slackEvents);
 
 setTimeout(function() {
 	countdownBot.initiateScheduler();
 }, 1000);
 
-
-function setup (countdownBot) {
-	console.log('setting up the old relax bot');
-	relax.on('message_new', function (data) {
-		console.log('in relax callback with a message', data);
-  		countdownBot.onMessage(data);
-	});
-
-	relax.on('disable_bot', data => {
-		console.log('time to delete a bot', data.team_uid);
-		Bot.remove({teamId: data.team_uid}).then(deleted => {
-			
-			// redis.multi()
-		 //      .hdel(process.env.RELAX_BOTS_KEY, data.team_uid)
-		 //      .exec();
-
-			console.log('removed bot', deleted, data.team_uid);
-
-
-		});
-	});
-
-	relax.start();
-
-	router(app, db, relax, countdownBot);
-	restartBots();
-}
-
-function restartBots () {
-	Bot.find({}).then(bots => {
-		let addedBots = {};
-
-		console.log(relax);
-	
-		console.log(`Found ${bots.length} bots`);
-		let done = 0;
-
-		_.forEach(bots, function (bot) {
-			// console.log('booting up:', bot.teamName, bot.teamId);
-
-			const botAccessToken = _.get(bot, 'botAccessToken');
-			const teamId = _.get(bot, 'teamId');
-
-			relax.createBot(teamId, botAccessToken);
-			addedBots[teamId] = true;
-			done++;
-
-			// console.log(`${done} out of ${bots.length} bots done. last: ${bot.teamName}`);
-		});
-	}).catch(function(err) {
-		console.log('error during boot:', err);
-	});
-}
 
 function isTestMode() {
 	return process.env.NODE_ENV === 'test_env';
--- router.js
+++ router.js
@@ -1,71 +1,82 @@
 const express = require('express'),
-		path = require('path'),
-		Bot = require('./lib/models/bot'),
-		_ = require('lodash'),
-		serveStatic = require('serve-static'),
-		CountdownBot = require('./lib/countdown');
-	    Slack = require('slack-node');
+		 path = require('path'),
+		  Bot = require('./lib/models/bot'),
+		    _ = require('lodash'),
+  serveStatic = require('serve-static'),
+ CountdownBot = require('./lib/countdown'),
+	SlackNode = require('slack-node');
+		slack = new SlackNode();	
+
+function eventIsLegit(event) {
+	return process.env.SLACK_VERIFICATION_TOKEN === event.token;
+}
+
+function handleSignup (req, res) {
+	slack.api('oauth.access', {
+		client_id: process.env.SLACK_CLIENT,
+		client_secret: process.env.SLACK_SECRET,
+		code: code
+	}, function (err, response) {
+		if (!!err || !response.bot) {
+			console.error(err);
+			res.status(500).sendFile(path.join(__dirname + '/public/error.html'));
+		} else {
+			const botAccessToken = response.bot.bot_access_token;
+			const botId = response.bot.bot_user_id;
+			const teamId = response.team_id;
+			const teamName = response.team_name;
+
+			Bot.findOne({teamId: teamId}).then(function (bot) {
+				if (bot) {
+					console.error('bot already exists for team', teamId);
+					res.sendFile(path.join(__dirname + '/public/oops.html'));
+				} else {
+					const bot = new Bot({
+						botAccessToken: botAccessToken,
+						userId: botId,
+						teamId: teamId,
+						teamName: teamName
+					});
+
+					bot.save().then(bot => {
+						CountdownBot.onSignupSucces(response, botAccessToken);
+						res.sendFile(path.join(__dirname + '/public/thanks.html'));
+					});
+				}
+			});
+		}
+	}); 
+}
 
 
-module.exports = function(app, db, relax, countdownBot) {
-	slack = new Slack();
-  	// Initializing route groups
+module.exports = function(app, db, countdownBot, slackEvents) {
+	// Initializing route groups
+	app.use("/", express.static(__dirname + '/public/'));
 
-  	app.use("/", express.static(__dirname + '/public/'));
+	app.get('/thanks', (req, res) => handleSignup(req, res));
 
-  	app.get('/thanks', (req, res) => {
-  		code = req.query.code;
-  		if (!code) {
-  			console.log('no code! why?!');
-  		}
-
-  		slack.api('oauth.access', {
-  			client_id: process.env.SLACK_CLIENT,
-  			client_secret: process.env.SLACK_SECRET,
-  			code: code
-  		}, function (err, response) {
-  			if (!!err || !response.bot) {
-  				console.error(err);
-  				res.status(500).sendFile(path.join(__dirname + '/public/error.html'));
-  			} else {
-  				const botAccessToken = response.bot.bot_access_token;
-  				const botId = response.bot.bot_user_id;
-  				const teamId = response.team_id;
-          const teamName = response.team_name;
-
-  				Bot.findOne({teamId: teamId}).then(function (bot) {
-  					if (bot) {
-              console.error('bot already exists for team', teamId);
-  						res.sendFile(path.join(__dirname + '/public/oops.html'));
-  					} else {
-  						const bot = new Bot({
-		  					botAccessToken: botAccessToken,
-		  					userId: botId,
-		  					teamId: teamId,
-                teamName: teamName
-		  				});
-
-		  				bot.save().then(bot => {
-		  					relax.createBot(teamId, botAccessToken);
-
-                console.log('response', response);
-                const channel = _.get(response, 'incoming_webhook.channel_id');
-                console.log('channel', channel);
-                if (channel) {
-                  countdownBot.hello({channel: channel}, botAccessToken);
-                } else {
-                  console.log('no channel for some reason!');
-                }
-
-		  					res.sendFile(path.join(__dirname + '/public/thanks.html'));
-		  				});
-  					}
-  				});
-  			}
-  		});
-  	});
-
-  	app.use('/*', (req, res) => {
+	app.use('/*', (req, res) => {
 		res.status(500).sendFile(path.join(__dirname + '/public/error.html'));
-  	});
+	});
+
+	// //\//\//\//\//\//\//\//\//\//\//\//\//\//\//\//\//\//\
+	// 					SLACK EVENT HANDLING
+	// /\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\
+
+	// Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
+
+	slackEvents.on('message', (event)=> {
+		console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
+	});
+
+	slackEvents.on('app_mention', (event)=> {
+		if (eventIsLegit(event)) {
+		 	CountdownBot.onMessage(event);
+		} else {
+			console.error('Token incorrect for event: ', event);
+		}
+	});
+
+	// Handle errors (see `errorCodes` export)
+	slackEvents.on('error', console.error);
 };

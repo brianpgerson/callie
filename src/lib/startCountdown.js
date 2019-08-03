311c666bf3dda56ccaa007e7901fd12ea9fbae2f
Brian Gerson <brianpgerson@gmail.com>
2019-08-03 12:16:27 -0700

    bracketology

--- src/lib/startCountdown.js
+++ src/lib/startCountdown.js
@@ -12,9 +12,9 @@ const notifyAlreadyCountdown = R.pipe(
   getEventFromSettings,
   event => notify(`There's already a countdown for ${event}`)
 );
-
+const notifyNoBrackets = notify(`Please don't use "<" and ">" in your settings. I used them to make it clear what YOUR inputs should be, but if you keep them in your event name, for example, it'll be hard to find your event later!`);
 const notifyNeedDateAndEvent = notify('You must set both an event and a date.');
-
+const usedBrackets = ({ settings: { event }}) => (R.indexOf('&lt;', event) === 0 && R.indexOf('&gt;', event) > 0);
 const saveCountdownAndNotifyResults = async (configuration) => {
   const countdown = await saveCountdown(configuration);
   return R.ifElse(
@@ -29,6 +29,12 @@ const startCountdown = async (configuration) => {
     notifyNeedDateAndEvent(configuration);
     return;
   }
+
+  if (usedBrackets(configuration)) {
+    notifyNoBrackets(configuration);
+    return;
+  }
+  
   
   const alreadyCountdown = await findCountdown(configuration);
   R.ifElse(
